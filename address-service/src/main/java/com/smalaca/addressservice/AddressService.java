21e7bafc5bee1c77813d24d0371473b153766ae5
smalaca <ab13.krakow@gmail.com>
2021-01-21 18:31:09 +0100

    Initial commit

---
+++ address-service/src/main/java/com/smalaca/addressservice/AddressService.java
@@ -0,0 +1,11 @@
+package com.smalaca.addressservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressService {
+    public static void main(String[] args) {
+        SpringApplication.run(AddressService.class, args);
+    }
+}
